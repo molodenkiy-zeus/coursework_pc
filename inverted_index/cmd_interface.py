6e1a280fd1636738d34f7133d5cfb684435538e7
molodenkiy-zeus <65317271+molodenkiy-zeus@users.noreply.github.com>
2023-01-15 11:22:53 +0200

    Update cmd_interface.py

--- inverted_index/cmd_interface.py
+++ inverted_index/cmd_interface.py
@@ -7,24 +7,26 @@ from inv_index import InvertedIndex
 class CmdInterface:
     def __init__(self, index: InvertedIndex):
         self.index = index
+        self.path = ''
 
     def run(self):
         print('index running')
         while True:
             cmd = input('>').split()
-            if cmd[0] == 'index':
-                if cmd[1] == 'add':
-                    self.__add(cmd)
-                elif cmd[1] == 'clear':
-                    self.index.clear()
-                    print(f'index empty')
-                elif cmd[1] == 'search':
-                    self.__search(cmd)
-                elif cmd[1] == 'stop':
-                    self.index.clear()
-                    break
-                else:
-                    print('Unknown command')
+            if cmd[0] == 'add_path':
+                self.__add(cmd)
+            elif cmd[0] == 'config_path':
+                self.path = cmd[1]
+            elif cmd[0] == 'add':
+                self.__add_v_config(cmd)
+            elif cmd[0] == 'clear':
+                self.index.clear()
+                print(f'index empty')
+            elif cmd[0] == 'search':
+                self.__search(cmd)
+            elif cmd[0] == 'stop':
+                self.index.clear()
+                break
             elif cmd[0] == 'help':
                 print('index add {path}\n'
                       'index clear\n'
@@ -34,15 +36,23 @@ class CmdInterface:
                 print('Unknown command')
 
     def __add(self, cmd):
-        path = cmd[2]
+        path = cmd[1]
         if os.path.exists(path):
             self.index.process(path)
             print(f'New files added')
         else:
-            raise FileNotFoundError(f'{path} does not exists')
+            print(f'{path} does not exists')
+
+    def __add_v_config(self, cmd):
+        path = os.path.join(self.path, cmd[1])
+        if os.path.exists(path):
+            self.index.process(path)
+            print(f'New files added')
+        else:
+            print(f'{path} does not exists')
 
     def __search(self, cmd):
-        keywords = cmd[2:]
+        keywords = cmd[1:]
         res_count = defaultdict(int)
         res = defaultdict(list)
         for word in keywords:
